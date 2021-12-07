a92cd2132b3631e054d57ab4c155b9e2c61e7aea
KwatMDPhD <kwat.me@icloud.com>
2021-12-06 23:47:57 -0700

    Find human genes

--- code/_.jl
+++ code/_.jl
@@ -19,3 +19,31 @@ using DictExtension
 using Gene
 using LeanProject
 using TableAccess
+
+function write_gene_by_method(
+    di::String,
+    or::String,
+    me_ge_::OrderedDict{String,Vector{String}},
+)::DataFrame
+
+    DictExtension.write(joinpath(di, "method_to_$(or)_genes.json"), sort(me_ge_))
+
+    ge_ = unique(vcat(values(me_ge_)...))
+
+    an_ge_me = DataFrame("Gene" => ge_)
+
+    for (me, se_) in me_ge_
+
+        an_ge_me[!, me] = [ge in se_ for ge in ge_]
+
+    end
+
+    an_ge_me[!, "Sum"] = vec(sum(Matrix(an_ge_me[!, 2:end]); dims = 2))
+
+    sort!(an_ge_me, "Sum"; rev = true)
+
+    TableAccess.write(joinpath(di, "$(or)_gene_by_method.tsv"), an_ge_me)
+
+    return an_ge_me
+
+end
