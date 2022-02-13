db08b84b2455baa1c8bcc3e79b8cefebb9333582
KwatMDPhD <kwat.me@icloud.com>
2022-02-13 16:03:46 -0700

    Recursive

--- code/_.jl
+++ code/_.jl
@@ -1,13 +1,13 @@
 # ---
 # Default lean project things
 
-using LeanProject
+#using LeanProject
 
 se = joinpath("..", "input", "setting.json")
 
-PAR, PAI, PAC, PAO = LeanProject.get_project_path(se)
+#PAR, PAI, PAC, PAO = LeanProject.get_project_path(se)
 
-SE = LeanProject.read_setting(se)
+#SE = LeanProject.read_setting(se)
 
 # ---
 # Project specific things
